544a7e668943dfbd2d73011f85bb5988784fb486
Anton Listkov <antonlistkov@gmail.com>
2024-03-02 13:46:57 +0100

    feat: create base server

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express, {Express} from 'express';
+import { Server } from 'node:http';
+
+export class App {
+    app: Express;
+    port: number;
+    server: Server;
+
+    constructor() {
+        this.app = express();
+        this.port = 8080;
+    }
+
+    public async init() {
+        this.app.get('/', (req, res) => {
+            res.send('Running...');
+        });
+        this.server = this.app.listen(this.port, () => {
+            console.log(`Server is running on http://localhost:${this.port}`)
+        });
+    }
+}
