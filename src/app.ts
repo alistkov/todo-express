75bb7b22959996663c228614cbc21ab17b03ec7e
Anton Listkov <antonlistkov@gmail.com>
2024-03-02 15:02:34 +0100

    fix: fix lint errors

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,22 @@
-import express, {Express} from 'express';
+import express, { Express } from 'express';
 import { Server } from 'node:http';
 
 export class App {
-    app: Express;
-    port: number;
-    server: Server;
+  app: Express;
+  port: number;
+  server: Server;
 
-    constructor() {
-        this.app = express();
-        this.port = 8080;
-    }
+  constructor() {
+    this.app = express();
+    this.port = 8080;
+  }
 
-    public async init() {
-        this.app.get('/', (req, res) => {
-            res.send('Running...');
-        });
-        this.server = this.app.listen(this.port, () => {
-            console.log(`Server is running on http://localhost:${this.port}`)
-        });
-    }
+  public async init(): Promise<void> {
+    this.app.get('/', (req, res) => {
+      res.send('Running...');
+    });
+    this.server = this.app.listen(this.port, () => {
+      console.log(`Server is running on http://localhost:${this.port}`);
+    });
+  }
 }
