2cc06af16d1f9fc54fe8397c7db116b61ebb3b4c
Anton Listkov <antonlistkov@gmail.com>
2024-03-03 19:31:24 +0100

    feat: request for db

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,24 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 import { router as todosRouter } from './todos/todos';
 
 const bootstrap = (): void => {
   const app = express();
   const port = 8080;
 
-  app.get('/', (req, res) => {
-    res.json({
-      message: 'Ok',
-    });
-  });
+  app.use(bodyParser.json());
 
-  app.use(todosRouter);
+  mongoose
+    .connect('mongodb://127.0.0.1:27017/todo-app')
+    .then(() => {
+      console.log('Connected to MongoDB');
+    })
+    .catch((error) => {
+      console.error('Failed to connect to MongoDB', error);
+    });
+
+  app.use('/api', todosRouter);
 
   app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
